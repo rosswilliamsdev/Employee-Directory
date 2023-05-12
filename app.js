2e14b6d87e24f6dbb65c0b3fe1456e87a490272b
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-12 11:35:40 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(name, email, city) {
+        this.name = name;
+        this.email = email;
+        this.city = city;
+    }
+}
+
+
+`<h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
