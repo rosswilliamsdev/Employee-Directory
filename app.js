e753a549a51f3a365646db415d503ce4c6452ba3
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-12 13:59:15 -0500

    image

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 class Employee {
-    constructor(name, email, city) {
+    constructor(image, name, email, city) {
         this.name = name;
         this.email = email;
         this.city = city;
+        this.image = image;
     }
 }
 
+const cards = document.querySelectorAll(".card");
 
-`<h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
+cards.forEach(card => {
+card.innerHTML = `<img src="${employee.image}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
+});
