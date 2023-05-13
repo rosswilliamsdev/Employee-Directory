bc6bbfeda2be03fa6b97d76d9fb243bea5593293
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-13 00:03:25 -0500

    fetch setup

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ class Employee {
 
 const cards = document.querySelectorAll(".card");
 
+ //Basic fetch request // to request 12 users add ?results=12
+fetch('https://randomuser.me/api/?exc=login')
+.then(response => response.json())
+.then(data => console.log(data))
+
+
 cards.forEach(card => {
-card.innerHTML = `<img src="${employee.image}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
+card.innerHTML = `<img src="${employee.picture.medium}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
 });
