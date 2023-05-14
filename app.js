bd35621e5ed1822a29555f8a09fca601d6102c25
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-13 20:38:39 -0500

    employee cards

--- app.js
+++ app.js
@@ -7,18 +7,32 @@ class Employee {
     }
 }
 
-const cards = document.querySelectorAll(".card");
+const card = document.querySelector(".card");
 
 // ////////////////////
 // FETCH FUNCTIONS 
 // ////////////////////
 
  //Basic fetch request // to request 12 users add ?results=12
-fetch('https://randomuser.me/api/?exc=login')
+fetch('https://randomuser.me/api/?exc=login/')
 .then(response => response.json())
-.then(data => console.log(data.results[0]))
+.then(data => console.log(data.results[0].picture.medium))
+
+// ////////////////////
+// HELPER FUNCTIONS
+// /////////////////////
+
+function generateImage(data){
+    const employeeCardPicture = `<img src="${data}" alt>`;
+    card.innerHTML = employeeCardPicture;
+}
 
 
-cards.forEach(card => {
-card.innerHTML = `<img src="${employee.picture.medium}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
-});
+
+
+
+
+
+// cards.forEach(card => {
+// card.innerHTML = `<img src="${employee.picture.medium}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
+// });
