daf6b11baff08229a00417406ccf86190e2e2014
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-15 14:27:17 -0500

    1

--- app.js
+++ app.js
@@ -19,14 +19,16 @@ fetch(url)
 .then(response => response.json())
 .then(data => employees.push(...data.results))
 
-
+console.log(employees[0].picture.medium)
 
 // Something I'm working on to add object properties to the cards
 cardsArray.forEach(card => {
+    for(let i=0; i < employees.length; i++){
         card.innerHTML += `<img src='${employees[i].picture.medium}'>`;
         card.innerHTML += `<p>${employees[i]}</p>`;
         card.innerHTML += `<p>${employees[i].location.city}</p>`;
         card.innerHTML += `<p>${employees[i].email}</p>`;
+    }
 });
 
 
