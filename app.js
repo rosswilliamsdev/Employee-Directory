a6b09159e2cb17fa44b6a7bc94876c90a7d62f60
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-16 14:05:05 -0500

    Preparing for modal

--- app.js
+++ app.js
@@ -1,55 +1,69 @@
-class Employee {
-    constructor(image, name, email, city) {
-        this.name = name;
-        this.email = email;
-        this.city = city;
-        this.image = image;
-    }
-}
-const url = 'https://randomuser.me/api/?results=12'
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
 const card = document.querySelector(".card");
 const cardsArray = Array.from(document.querySelectorAll(".card"));
+const gridContainer = document.querySelector('.grid-container');
+const modal = document.querySelector('.modal');
 let employees = [];
 
 // ////////////////////
 // FETCH FUNCTIONS 
 // ////////////////////
 
-fetch(url)
+
+
+fetch(urlAPI)
 .then(response => response.json())
-.then(data => employees.push(...data.results))
+.then(response => response.results)
+.then(displayEmployees)
+.catch(error => console.log(error));
 
-console.log(employees[0].picture.medium)
-
-// Something I'm working on to add object properties to the cards
-cardsArray.forEach(card => {
-    for(let i=0; i < employees.length; i++){
-        card.innerHTML += `<img src='${employees[i].picture.medium}'>`;
-        card.innerHTML += `<p>${employees[i]}</p>`;
-        card.innerHTML += `<p>${employees[i].location.city}</p>`;
-        card.innerHTML += `<p>${employees[i].email}</p>`;
-    }
-});
 
 
 // ////////////////////
 // HELPER FUNCTIONS
 // /////////////////////
 
-function generateImage(data){
-    const employeeCardPicture = `<img src="${data}" alt>`;
-    cardsArray.forEach(card => {
-        card.innerHTML += employeeCardPicture;
-    })
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+
+        employeeHTML += `
+        <div class="card" data-index="${index}">
+        <img class="avatar" src="${picture.large}"/>
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+        </div>
+        </div>
+        `
+    });
+gridContainer.innerHTML += employeeHTML;
 }
 
 
+//////////////////
+// EVENT LISTENERS
+//////////////////
+
+card.addEventListener('click', () => {
+
+})
 
 
+// I was told this didn't work because the promise from the fetch wasn't fulfilled by the time this function started, so there were no objects inside the employees array. When this was live, it would return employee[i] is undefined. 
 
-
-
-
-// cards.forEach(card => {
-// card.innerHTML = `<img src="${employee.picture.medium}"/><h2>${employee.name}</h2><p>${employee.email}</p><p>${employee.city}</p>`
+// cardsArray.forEach(card => {
+//     for(let i=0; i < employees.length; i++){
+//         card.innerHTML += `<img src='${employees[i].picture.medium}'>`;
+//         card.innerHTML += `<p>${employees[i]}</p>`;
+//         card.innerHTML += `<p>${employees[i].location.city}</p>`;
+//         card.innerHTML += `<p>${employees[i].email}</p>`;
+//     }
 // });
