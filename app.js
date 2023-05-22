a2cc22733876fba2d6868983256ab2baa781872b
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-05-22 13:29:07 -0500

    svg folder

--- app.js
+++ app.js
@@ -67,6 +67,16 @@ function createModal(index) {
     let modalHTML = `
     <span class="close-btn">&times;</span>
     <img class="avatar" src="${picture.large}" />
+    <button class='previous'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <polygon points="15.293 3.293 6.586 12 15.293 20.707 16.707 19.293 9.414 12 16.707 4.707 15.293 3.293"/>
+</svg>
+    </button>
+    <button class='next'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <polygon points="7.293 4.707 14.586 12 7.293 19.293 8.707 20.707 17.414 12 8.707 3.293 7.293 4.707"/>
+</svg>
+    </button>
     <div class="modal-text-container">
         <h2 class='name'>${name.first} ${name.last}</h2>
         <p class="email">${email}</p>
